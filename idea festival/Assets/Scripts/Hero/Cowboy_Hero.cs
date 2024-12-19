17c8a50a0af178a8a0cb1ead1a33da51af3cc17e
fore4022 <s23009@gsm.hs.kr>
2024-12-19 09:03:52 +0900

    Cowboy_Hero
    Cowboy_Hero 구현 완료

--- idea festival/Assets/Scripts/Hero/Cowboy_Hero.cs
+++ idea festival/Assets/Scripts/Hero/Cowboy_Hero.cs
@@ -8,15 +8,16 @@ public class Cowboy_Hero : Controller
     [SerializeField]
     private Projectile[] skill1_Projectile = new Projectile[] { };
     [SerializeField]
-    private Projectile[] skill2_Projectile = new Projectile[] { };
+    private Projectile[] skill3_Projectile = new Projectile[] { };
     [SerializeField]
     private AttackRange skill2_Range;
+    [SerializeField]
+    private AttackRange skill4_Range;
 
     private bool skill1 = false;
     private bool skill2 = false;
     private bool skill3 = false;
     private bool skill4 = false;
-    private bool skill5 = false;
 
     protected override void DefaultAttack()
     {
@@ -27,6 +28,7 @@ public class Cowboy_Hero : Controller
         base.Awake();
 
         skill2_Range.Init(gameObject, so.skills[1].damage);
+        skill4_Range.Init(gameObject, so.skills[3].damage);
     }
     public override void ButtonY(InputValue value)
     {
@@ -64,15 +66,6 @@ public class Cowboy_Hero : Controller
 
         Skill(Skill4, "skill4", so.skills[3], ref skill4);
     }
-    public override void RightTrigger(InputValue value)
-    {
-        if(isJump)
-        {
-            return;
-        }
-
-        Skill(Skill1, "skill5", so.skills[4], ref skill5);
-    }
     public void Skill1()
     {
         ActiveProjectile(skill1_Projectile);
@@ -83,15 +76,11 @@ public class Cowboy_Hero : Controller
     }
     public void Skill3()
     {
-        ActiveProjectile(skill2_Projectile);
+        ActiveProjectile(skill3_Projectile);
     }
     public void Skill4()
     {
-
-    }
-    public void Skill5()
-    {
-
+        StartCoroutine(Casting_Skill4());
     }
     private IEnumerator Casting_DefaultAttack()
     {
@@ -110,5 +99,13 @@ public class Cowboy_Hero : Controller
         yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1);
 
         skill2_Range.gameObject.SetActive(false);
+    }
+    private IEnumerator Casting_Skill4()
+    {
+        skill4_Range.gameObject.SetActive(true);
+
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.6);
+        
+        skill4_Range.gameObject.SetActive(false);
     }
 }
